546403f8debe2718324a8d1b49fc99368ba59ffd
MathBrandino <m.v.brandino@gmail.com>
2020-02-29 16:45:44 -0300

    primeiro dia

---
+++ app/src/main/java/br/com/caelum/twittelum/ListaTweetsActivity.kt
@@ -0,0 +1,35 @@
+package br.com.caelum.twittelum
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.lista_tweets_activity.*
+
+class ListaTweetsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.lista_tweets_activity)
+
+        val tweets = arrayListOf(
+            "Hoje deu ruim nos pcs :(",
+            "Semana que vem não vai dar ruim ( eu espero )",
+            "Sinto fome",
+            "Kotlin é legal",
+            "Sei la",
+            "Outro sei la",
+            "zzzZZZzzZZz"
+        )
+
+        listaTweets.adapter =
+            ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, tweets)
+
+
+        btnNewTweet.setOnClickListener {
+            val intencao = Intent(this, MainActivity::class.java)
+            startActivity(intencao)
+        }
+    }
+
+}
