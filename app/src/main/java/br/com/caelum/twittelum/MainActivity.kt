54785a19cf4018c9c2ca27e64d77d64dd547d1e5
Matheus Brandino <m.v.brandino@gmail.com>
2020-03-07 10:16:33 -0300

    menu

--- app/src/main/java/br/com/caelum/twittelum/MainActivity.kt
+++ app/src/main/java/br/com/caelum/twittelum/MainActivity.kt
@@ -1,7 +1,8 @@
 package br.com.caelum.twittelum
 
-import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
@@ -11,14 +12,30 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
 
-        publicarTweet.setOnClickListener {
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.tweet_menu, menu)
+        return true
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean = when (item.itemId) {
+        R.id.menuPublicarTweet -> {
             val conteudo = tweetConteudo.text.toString()
             Toast.makeText(this, conteudo, Toast.LENGTH_LONG).show()
-
             finish()
+            true
         }
 
+        android.R.id.home -> {
+            finish()
+            true
+        }
+        else -> false
     }
+
+
 }
 
